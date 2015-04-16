3b29a5023ded40fb01dd94ff7e74d171bf903506
csokiskeksz <csokisskeksz@gmail.com>
2015-04-16 23:31:30 +0200

    TimerTask-Notifier
    MyTimerTask basic implementation,deleting extra class

--- source/mars/MyTimerTask.java
+++ source/mars/MyTimerTask.java
@@ -1,5 +1,7 @@
 package mars;
 
+import java.util.Observable;
+import java.util.Observer;
 import java.util.TimerTask;
 
 public class MyTimerTask extends TimerTask {
@@ -7,35 +9,39 @@ public class MyTimerTask extends TimerTask {
 	private int gametime;
 	private int interval;
 	private int timeElapsed;
-	private Notifier notifier;
+	private Observable notifier;
 
 	public boolean isOver() {
-		
+		if((gametime-timeElapsed)<0)
+			return true;
+		else 
+			return false;
 	}
 
 	public void reset() {
-		
+		enabled=true;
+		timeElapsed=0;
 	}
 
 	public void setEnabled(boolean value) {
-		
+		enabled=value;
 	}
 
 	public void setGameTime(int gt) {
-		
+		gametime=gt;
 	}
 
 	public void setInterval(int interval) {
-		
+		this.interval=interval;
 	}
 
-	public void registerObserver(Object robot) {
-		
+	public void registerObserver(Observer robot) {
+		notifier.addObserver(robot);
 	}
 
 	@Override
 	public void run() {
-		
-		
+		//timeElapsed++; //Ez még nem igazán biztos
+		notifier.notifyAll();
 	}
 }
--- source/mars/Notifier.java
+++
@@ -1,7 +0,0 @@
-package mars;
-
-import java.util.Observable;
-
-public class Notifier extends Observable {
-	
-}
