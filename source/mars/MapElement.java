1461d816d62af9436b63f7ba20c8b86b10edaf62
khongi <b.khongi@gmail.com>
2015-04-20 12:04:01 +0200

    Ütközés detektálás megoldva
    (elvileg...)

--- source/mars/MapElement.java
+++ source/mars/MapElement.java
@@ -31,22 +31,51 @@ public class MapElement {
 	public boolean isFree(){
 		return (refPlayer.isEmpty() && refMM.isEmpty() && spot==null);
 	}
-
-	private void playerPlayer(){
-		
-	}
 	
-	private void playerMM(){
-		
-	}
-	
-	private void mmMM(){
-		
+	private void collidePlayers(){
+		int maxspeed = 0;
+		int sum = 0;
+		for(Player p : refPlayer){
+			int temp = p.getSpeed();
+			sum += temp;
+			if(temp>maxspeed)
+				maxspeed = temp;
+		}
+		int avg = sum/refPlayer.size();
+		for(Player p : refPlayer){
+			if(p.getSpeed() != maxspeed)
+				p.setAlive(false);
+			else
+				p.setSpeed(avg);
+		}
 	}
 	
 	public void handleCollision() {
-		//TODO
-		clearRobotRefs();
+		//Van-e jatekos
+		if(!refPlayer.isEmpty()){
+			//Ha van akkor mindenkeppen meghalnak a kisrobotok
+			for(MicroMachine mm : refMM){
+				mm.setAlive(false);
+			}
+			//Utkoztetjuk a jatekosokat, egy jatekosra is mukodik
+			collidePlayers();
+			//Torolni kell a referenciakat, az else ag miatt
+			clearRobotRefs();
+		}
+		//Nem volt jatekos
+		else{
+			//Van-e kisrobot
+			if(!refMM.isEmpty()){
+				//Csak akkor van utkozes ha legalabb 2 kisrobot van
+				if(refMM.size()>1)
+					//Ha van legalabb 2, akkor mindenkit visszaforditunk (stay eseten se lesz gond)
+					for(MicroMachine mm : refMM){
+						mm.invertDir();
+					}
+			}
+			//Toroljuk mindenkeppen a referenciakat
+			clearRobotRefs();
+		}
 	}
 
 	public boolean hasSpot() {
