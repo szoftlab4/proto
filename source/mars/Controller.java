258c147ddbf18d3f86fdc5076eacf5902ce3b9e2
khongi <b.khongi@gmail.com>
2015-04-27 21:12:46 +0200

    dsgx

--- source/mars/Controller.java
+++ source/mars/Controller.java
@@ -4,16 +4,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 public class Controller implements MouseListener, ActionListener{
 
+	View view;
 	
 	public Controller(){
-		
+		view = new View();
 	}
 	
 	public void drawGame(){
-		
+		view.repaint();
+	}
+	
+	public void addMap(ArrayList<MapElement> map){
+		view.initMap(map);
 	}
 	
 	@Override
