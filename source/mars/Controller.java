472cbc4c09822afcbdee98a0507f852ac6fbd2e7
khongi <b.khongi@gmail.com>
2015-05-08 00:15:16 +0200

    Gabor pls

--- source/mars/Controller.java
+++ source/mars/Controller.java
@@ -2,11 +2,11 @@ package mars;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class Controller implements MouseListener, ActionListener{
+public class Controller implements KeyListener, ActionListener{
 
 	View view;
 	Game game;
@@ -61,24 +61,8 @@ public class Controller implements MouseListener, ActionListener{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
-<<<<<<< HEAD
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-=======
 	public void keyPressed(KeyEvent arg0) {
 		int keycode = arg0.getKeyCode();
 		switch(keycode){
@@ -135,18 +119,17 @@ public class Controller implements MouseListener, ActionListener{
 			default:
 				break;
 		}
->>>>>>> origin/master
 		
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
+	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
